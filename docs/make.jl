72cdf632117db85e60a90190cd7e9eba717253e3
Flemming <holtorf@mit.edu>
2021-07-06 13:10:54 -0400

    fixed typo docs src files

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,16 @@
-using MarkovBoundsSOS
+using MarkovBounds
 using Documenter
 
-DocMeta.setdocmeta!(MarkovBoundsSOS, :DocTestSetup, :(using MarkovBoundsSOS); recursive=true)
+DocMeta.setdocmeta!(MarkovBounds, :DocTestSetup, :(using MarkovBounds); recursive=true)
 
 makedocs(;
-    modules=[MarkovBoundsSOS],
+    modules=[MarkovBounds],
     authors="Flemming Holtorf",
-    repo="https://github.com/FHoltorf/MarkovBoundsSOS.jl/blob/{commit}{path}#{line}",
-    sitename="MarkovBoundsSOS.jl",
+    repo="https://github.com/FHoltorf/MarkovBounds.jl/blob/{commit}{path}#{line}",
+    sitename="MarkovBounds.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://FHoltorf.github.io/MarkovBoundsSOS.jl",
+        canonical="https://FHoltorf.github.io/MarkovBounds.jl",
         assets=String[],
     ),
     pages=[
@@ -19,5 +19,5 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/FHoltorf/MarkovBoundsSOS.jl",
+    repo="github.com/FHoltorf/MarkovBounds.jl",
 )
