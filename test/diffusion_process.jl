923e55735a046af0306583bcb906bd3f004b2d81
Flemming Holtorf <holtorf@mit.edu>
2022-06-21 14:50:05 -0400

    adjust coefficients in diffusion test for simpler reproduction

--- test/diffusion_process.jl
+++ test/diffusion_process.jl
@@ -3,34 +3,34 @@ using Symbolics, LinearAlgebra, MarkovBounds
 # initialization via Symbolics.jl
 @variables x, y, u, v
 f = [1.5*x*(x-1)^2*u-v^2; 1.2*y+(u+0.2)^2]
-σ = Matrix(Diagonal([0.2*x^2; 0.27*y]))
-X = [1.2*(x^2 + 0.21*y)^2 >= 1, (0.21*x - 0.53*y)^2 <= 2.0*x^3]
+σ = Matrix(Diagonal([0.2*x^2; 0.2*y]))
+X = [1.2*(x^2 + 0.2*y)^2 >= 1, (0.2*x - 0.5*y)^2 <= 2.0*x^3]
 dp1 = DiffusionProcess([x,y], f, σ, X, controls = [u,v])
 
 @variables x[1:2], u[1:2]
 f = [1.5*x[1]*(x[1]-1)^2*u[1]-u[2]^2; 1.2*x[2]+(u[1]+0.2)^2]
-σ = Matrix(Diagonal([0.2*x[1]^2; 0.27*x[2]]))
-X = [1.2*(x[1]^2 + 0.21*x[2])^2 >= 1, (0.21*x[1] - 0.53*x[2])^2 <= 2.0*x[1]^3]
+σ = Matrix(Diagonal([0.2*x[1]^2; 0.2*x[2]]))
+X = [1.2*(x[1]^2 + 0.2*x[2])^2 >= 1, (0.2*x[1] - 0.5*x[2])^2 <= 2.0*x[1]^3]
 dp2 = DiffusionProcess(x, f, σ, X, controls = u)
 
 @variables x[1:2]
 f = [1.5*x[1]*(x[1]-1)^2; 1.2*x[2]]
-σ = Matrix(Diagonal([0.2*x[1]^2; 0.27*x[2]]))
-X = [1.2*(x[1]^2 + 0.21*x[2])^2 >= 1, (0.21*x[1] - 0.53*x[2])^2 <= 2.0*x[1]^3]
+σ = Matrix(Diagonal([0.2*x[1]^2; 0.2*x[2]]))
+X = [1.2*(x[1]^2 + 0.2*x[2])^2 >= 1, (0.2*x[1] - 0.5*x[2])^2 <= 2.0*x[1]^3]
 dp2 = DiffusionProcess(x, f, σ, X)
 
 @variables x[1:2], u[1:2]
 x = collect(x)
 u = collect(u)
 f = [1.5*x[1]*(x[1]-1)^2*u[1]-u[2]^2; 1.2*x[2]+(u[1]+0.2)^2]
-σ = Matrix(Diagonal([0.2*x[1]^2; 0.27*x[2]]))
-X = [1.2*(x[1]^2 + 0.21*x[2])^2 >= 1, (0.21*x[1] - 0.53*x[2])^2 <= 2.0*x[1]^3]
+σ = Matrix(Diagonal([0.2*x[1]^2; 0.2*x[2]]))
+X = [1.2*(x[1]^2 + 0.2*x[2])^2 >= 1, (0.2*x[1] - 0.5*x[2])^2 <= 2.0*x[1]^3]
 dp3 = DiffusionProcess(x, f, σ, X, controls = u)
 
 # specification via DynamicPolynomials
 @polyvar x[1:2]
 @polyvar u[1:2]
 f = [1.5*x[1]*(x[1]-1)^2*u[1]-u[2]^2; 1.2*x[2]+(u[1]+0.2)^2]
-σ = Matrix(Diagonal([0.2*x[1]^2; 0.27*x[2]]))
-X = @set(0.2*(x[1]^2 + 0.21*x[2])^2 - 1 >= 0 && (0.21*x[1] - 0.53*x[2])^2 - 2.0*x[1]^3 >= 0)
+σ = Matrix(Diagonal([0.2*x[1]^2; 0.2*x[2]]))
+X = @set(0.2*(x[1]^2 + 0.2*x[2])^2 - 1 >= 0 && (0.2*x[1] - 0.5*x[2])^2 - 2.0*x[1]^3 >= 0)
 dp4 = DiffusionProcess(x, f, σ, X, controls = u)
