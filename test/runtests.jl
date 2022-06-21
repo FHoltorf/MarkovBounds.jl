0081af2623eaff0115027a0762b12da1e2a39b90
Flemming Holtorf <holtorf@mit.edu>
2022-06-21 18:17:14 -0400

    include tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,6 @@ using Test
 include("reaction_process.jl")
 include("langevin_process.jl")
 include("control_process.jl")
+include("jump_process.jl")
+include("diffusion_process.jl")
+include("jumpdiffusion_process.jl")
